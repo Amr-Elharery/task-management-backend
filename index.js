62cae07eb367a0595464ec045b775fb0bb91e477
Amr Elharery <amrelharery@gmail.com>
2026-08-21 18:55:55 +0300

    Add Cookie Parser

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
+
 
 // Swagger setup
 const swaggerUi = require('swagger-ui-express');
@@ -26,6 +28,7 @@ const corsOptions = {
 // Middlewares
 app.use(cors(corsOptions));
 app.use(express.json());
+app.use(cookieParser());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use('/api/auth', authRoutes);
